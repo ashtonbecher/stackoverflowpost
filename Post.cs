78501868c16225dbd3bb5d1451178f26aeac9554
Ashton Becher <ash.becher@gmail.com>
2021-05-24 21:35:45 -0500

    Add project files.

---
+++ Post.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackOverflow_Post
+{
+    public class Post
+    {
+        public string Title;
+        public string Description;
+        private readonly DateTime _postTime;
+        private int _upvotes { get; set; }
+        private int _downvotes { get; set; }
+
+        public Post()
+        {
+            this._postTime = DateTime.Now;
+        }
+
+        public void Upvote()
+        {
+            _upvotes += 1;
+        }
+
+        public void Downvote()
+        {
+            _downvotes += 1;
+        }
+
+        public int GetVoteValue()
+        {
+            return _upvotes - _downvotes;
+        }
+
+        public DateTime GetPostTime()
+        {
+            return _postTime;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StackOverflow_Post
+{
+
+    // This is a VERY simple program to simply demonstrate my understanding of classes
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post 
+                        { Title = "Intermediate C# is awesome!",
+                          Description = "I learned a lot in this class. Does anyone else agree?"};
+            for (int i = 0; i < 5; i++)
+            {
+                post.Upvote();
+            }
+            for (int j = 0; j < 2; j++)
+            {
+                post.Downvote();
+            }
+            Console.WriteLine(post.Title);
+            Console.WriteLine(post.Description);
+            Console.WriteLine("Post was posted at {0}", post.GetPostTime());
+            Console.WriteLine("Aggregate vote count: {0}", post.GetVoteValue());
+            Console.ReadLine();
+        }
+    }
+
+}
